a9545e496882e43460f8e0ecca9debc30cb41adb
Matthew Moyer <matthewmoyer@Matthews-MacBook-Pro.local>
2016-12-27 22:01:00 -0500

    add game setup functions and content

--- script.js
+++ script.js
@@ -1,26 +1,64 @@
 $(document).ready(function(){
 
+//global variables
 var submitButton = document.getElementById("target-word-submit-button");
-
-
+var targetWordInput = document.getElementById("target-word-input");
+var targetWord;
+var targetWordLength;
+var targetArray;
 
 //hangman module
 
 var Hangman = (function(){
+
+	//get the target word from input
 	var _privateGetTargetWordFunction = function(){
-		var submitButton = document.getElementById("target-word-submit-button");
-		var x = document.getElementById("target-word-input").value;
-		console.log(x);
+		//set target word to value of input
+		targetWord = document.getElementById("target-word-input").value;
 	}
 
+	var _privateGetLength = function(){
+		targetWordLength = targetWord.length;
+		console.log(targetWordLength);
+	}	
+
+	var _privateCreateArray = function(){
+		targetArray = targetWord.split("");
+		console.log(targetArray);
+	}
+
+	var _privateHideInput = function(){
+		targetWordInput.classList.add("hide");
+	}
+
+	var _privateHideSubmitButton = function(){
+		submitButton.classList.add("hide");
+	}
+
+
+
 	return{
-		getGuess: function(){
+		//public function to get target word
+		pickWord: function(){
 			_privateGetTargetWordFunction();
+			_privateGetLength();
+			_privateHideInput();
+			_privateHideSubmitButton();
+			_privateCreateArray();
 		}
 	}
 })();
 
+
+
+
+
 submitButton.addEventListener("click", function(){
-	Hangman.getGuess();
+	Hangman.pickWord();
+	console.log(targetWord);
 });
+
+
+
+
 });
